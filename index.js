8428df6942cccdb0daf46daeff55181db5c38de7
TheVivekYadav <vivekyadavclasher@gmail.com>
2024-10-17 13:14:48 +0000

    create index.js and included basic routes

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8080;
+
+app.get('/',(req, res) => {
+	res.send("Hello World");
+});
+
+app.get('greet', (req, res) => {
+	res.send("Hello Developers");
+});
+
+app.listen(port, () => {
+	console.log(`Server is running at port: {$port}`);
+});
